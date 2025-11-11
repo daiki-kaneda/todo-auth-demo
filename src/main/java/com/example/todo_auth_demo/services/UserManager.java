a33dc1ff8f446c0b4565899ef282b9e41a8b7db4
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-12 00:53:46 +0900

    Add UserManager

---
+++ src/main/java/com/example/todo_auth_demo/services/UserManager.java
@@ -0,0 +1,30 @@
+package com.example.todo_auth_demo.services;
+
+import org.springframework.stereotype.Service;
+
+import com.example.todo_auth_demo.drivers.FirebaseAuthDriver;
+import com.example.todo_auth_demo.models.User;
+import com.example.todo_auth_demo.repositories.UserRepository;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+@Service
+public class UserManager {
+    private final UserRepository userRepository;
+
+    public UserManager(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User loadOrCreateUser(String idToken) throws FirebaseAuthException {
+        FirebaseToken token = FirebaseAuthDriver.verifyIdToken(idToken);
+        return userRepository.findById(token.getUid())
+                .orElseGet(() -> {
+                    User newUser = new User();
+                    newUser.setUid(token.getUid());
+                    newUser.setName((String) token.getClaims().get("name"));
+                    return userRepository.save(newUser);
+                });
+    }
+
+}
