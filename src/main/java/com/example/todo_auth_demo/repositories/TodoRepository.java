3aece818a7995be79fb571319ccd8daebd69c0d1
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-13 02:07:48 +0900

    Modify TodoRepository

--- src/main/java/com/example/todo_auth_demo/repositories/TodoRepository.java
+++ src/main/java/com/example/todo_auth_demo/repositories/TodoRepository.java
@@ -1,15 +1,14 @@
 package com.example.todo_auth_demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.repository.query.Param;
 
 import com.example.todo_auth_demo.models.Todo;
 
 import java.util.List;
 
-
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    @Query("SELECT t FROM TODO t WHERE t.user.id = :uid ORDER BY t.id DESC")
+    @NativeQuery("SELECT * FROM todo t WHERE t.user_uid = :uid ORDER BY t.id DESC")
     List<Todo> findByUserUid(@Param("uid") String uid);
 }
