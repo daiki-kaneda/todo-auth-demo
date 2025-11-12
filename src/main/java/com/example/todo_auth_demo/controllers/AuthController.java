8d480f444b33a1987731f879890aca7ad14b330f
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-13 00:50:11 +0900

    Add AuthController to handle request to /auth to sign up or login

---
+++ src/main/java/com/example/todo_auth_demo/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.todo_auth_demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todo_auth_demo.dtos.LoginRequest;
+import com.example.todo_auth_demo.dtos.LoginResponse;
+import com.example.todo_auth_demo.models.User;
+import com.example.todo_auth_demo.services.UserManager;
+import com.google.firebase.auth.FirebaseAuthException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final UserManager userManager;
+
+    public AuthController(UserManager userManager) {
+        this.userManager = userManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) throws FirebaseAuthException {
+        User user = userManager.loadOrCreateUser(request.idToken());
+
+        return ResponseEntity.ok(new LoginResponse(user.getUid(), "Login success!"));
+    }
+}
---
+++ src/main/java/com/example/todo_auth_demo/dtos/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.example.todo_auth_demo.dtos;
+
+public record LoginResponse(String uid, String message) {
+}
