420a20fe7c90ae5ae54e76a83f3f50b4596ec564
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-13 01:28:09 +0900

    Modify TodoRepository to add findByUserUid

--- src/main/java/com/example/todo_auth_demo/repositories/TodoRepository.java
+++ src/main/java/com/example/todo_auth_demo/repositories/TodoRepository.java
@@ -1,7 +1,15 @@
 package com.example.todo_auth_demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.todo_auth_demo.models.Todo;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {}
+import java.util.List;
+
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+    @Query("SELECT t FROM TODO t WHERE t.user.id = :uid ORDER BY t.id DESC")
+    List<Todo> findByUserUid(@Param("uid") String uid);
+}
