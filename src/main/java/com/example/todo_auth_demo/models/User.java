1fad28e63bc791eb15003107fe43fe0b41af711d
daiki-kaneda <daiki0622developing@gmail.com>
2025-11-13 23:58:44 +0900

    Rename table name:user->users to fix SQL error(user is reserved word)

--- src/main/java/com/example/todo_auth_demo/models/User.java
+++ src/main/java/com/example/todo_auth_demo/models/User.java
@@ -7,10 +7,12 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     private String uid;
